40e4d34a522820d8348145f63ffafe8106ec9ff0
Danielle Bergbower <dkw5011@gmail.com>
2019-04-21 11:17:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,29 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new << 5
+  array << 5
 end
 
 def add_element_to_start_of_array(array, element)
-  new.unshift(0)
+  array.unshift(0)
 end
 
 def remove_element_from_end_of_array(array)
-  new.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  new.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  new[2]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  new[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  new[-1]
+  array[-1]
 end
